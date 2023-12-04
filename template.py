9c5777f73cfdce7630b3dd956ae422d90885ae06
Rob Hailman <robhailman@gmail.com>
2023-12-04 10:09:18 -0500

    Add boilerplate template
    It's not much, but it's in every day's solution so might as well have it
    somewhere.

---
+++ template.py
@@ -0,0 +1,11 @@
+from time import perf_counter
+
+IN_FILE = "##/input.txt"
+
+if __name__ == "__main__":
+    start_time = perf_counter()
+
+    # solution goes here
+
+    end_time = perf_counter()
+    print(f"Execution time: {end_time-start_time:.3f}s")
